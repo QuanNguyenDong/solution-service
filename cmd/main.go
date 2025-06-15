d06e2de3308b4a5da9b54884d2208c86435c62f4
Quan Quan <79500388+QuanNguyenDong@users.noreply.github.com>
2025-06-15 11:31:55 +1000

    new deployment

--- cmd/main.go
+++ cmd/main.go
@@ -8,38 +8,10 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-// var (
-// 	client *s3.Client
-// )
-
-// func init() {
-// 	cfg, err := config.LoadDefaultConfig(context.TODO())
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	client = s3.NewFromConfig(cfg)
-// }
-
-// func listBuckets() {
-// 	output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
-// 		Bucket: aws.String("lc-solutions-bucket"),
-// 	})
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	log.Println("first page results")
-// 	for _, object := range output.Contents {
-// 		log.Printf("key=%s size=%d", aws.ToString(object.Key), *object.Size)
-// 	}
-// }
-
 func main() {
 	router := chi.NewRouter()
 
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		// listBuckets()
 		fmt.Fprintln(w, "Hello World")
 	})
 
